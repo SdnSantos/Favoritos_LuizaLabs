c3d7a4ec0fcb495491c306f10a44245a0f83cea1
Sidney Santos <sidney_ssantos@hotmail.com>
2020-03-07 14:52:08 -0300

    Configurações do Eslint, Prettier e EditorConfig

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ msg: 'Hello World' })
+  return res.json({ msg: 'Hello World' });
 });
 
 export default routes;
